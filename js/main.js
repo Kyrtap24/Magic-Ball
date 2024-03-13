a5157622339bc836398d50fa5c93d706cc399e13
Kyrtap24 <patryktupacz@gmail.com>
2024-03-13 10:53:26 +0100

    Fixed script JS

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const answerOptions = [
 	'Oczywiście, jeszcze jak!',
 	'Na pewno, na pewno, czas pokaże, zobaczymy, zobaczymy...',
 	'Skąd Ci to przyszło do głowy?',
-	'Wstawaj zesrałeś się!',
+	'No a jak!',
 	'Jeden rabin powie tak, drugi rabin powie nie',
 	'Nie wiem, choć sie domyślam',
 ]
@@ -20,17 +20,15 @@ const shakeBall = () => {
 const checkInput = () => {
 	if (input.value !== '' && input.value.slice(-1) === '?') {
 		showRandomAnswer()
-		ball.classList.remove('shake-animation')
 		error.textContent = ''
 	} else if (input.value !== '' && input.value.slice(-1) !== '?') {
 		error.textContent = 'Pytanie musi być zakończone znakiem "?".'
 		answer.textContent = ''
-		ball.classList.remove('shake-animation')
 	} else {
 		error.textContent = 'Wprowadź pytanie!'
 		answer.textContent = ''
-		ball.classList.remove('shake-animation')
 	}
+    ball.classList.remove('shake-animation')
 }
 
 const showRandomAnswer = () => {
